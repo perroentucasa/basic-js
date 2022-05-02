ce90be8eec7b8b5b486653c099a628cc63406ecb
perroentucasa <86454571+perroentucasa@users.noreply.github.com>
2022-05-02 20:44:16 +0300

    feat: cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -21,12 +21,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class VigenereCipheringMachine {
   encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!message || !key) {
+      throw new Error();
+    }
+
+    message = message.toUpperCase().split("");
+    key = key.toUpperCase().split("");
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (ABC.indexOf(message[i]) != -1) {
+        message[i] =
+          ABC[(ABC.indexOf(message[i]) + ABC.indexOf(key[j])) % ABC.length];
+        j = ++j % key.length;
+      }
+    }
+    return this.trueOrFalse ? message.join("") : message.reverse().join("");
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!encryptedMessage || !key) {
+      throw new Error();
+    }
+
+    key = key.toUpperCase().split("");
+    encryptedMessage = encryptedMessage.toUpperCase().split("");
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if (ABC.indexOf(encryptedMessage[i]) != -1) {
+        encryptedMessage[i] =
+          ABC[
+            (ABC.indexOf(encryptedMessage[i]) +
+              ABC.length -
+              ABC.indexOf(key[j])) %
+              ABC.length
+          ];
+        j = ++j % key.length;
+      }
+    }
+    return this.trueOrFalse
+      ? encryptedMessage.join("")
+      : encryptedMessage.reverse().join("");
   }
 }
 
