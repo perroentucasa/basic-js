bc8d800218e677c63c3e28648bc6a8ba8aefefe3
perroentucasa <86454571+perroentucasa@users.noreply.github.com>
2022-05-02 21:10:15 +0300

    feat: solution repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+ let resultChain = [];
+  let defSeparator = "";
+  let repeatElement = "";
+  let addSeparator = "";
+  let addRepeatTimes;
+
+  if (!options) {
+    resReturn = str;
+    return resReturn;
+  } else {
+    !options.separator
+      ? (defSeparator = "+")
+      : (defSeparator = options.separator);
+
+    !options.additionSeparator
+      ? (addSeparator = "|")
+      : (addSeparator = options.additionSeparator);
+
+    if (options.hasOwnProperty("addition")) {
+      !options.additionRepeatTimes
+        ? (addRepeatTimes = 1)
+        : (addRepeatTimes = options.additionRepeatTimes);
+
+      for (let s = 0; s < addRepeatTimes; s++) {
+        addRepeatTimes > 1 && s < addRepeatTimes - 1
+          ? (repeatElement = repeatElement + options.addition + addSeparator)
+          : (repeatElement = repeatElement + options.addition);
+      }
+
+      str = str + repeatElement;
+    }
+    if (!options.repeatTimes) {
+      options.repeatTimes = 1;
+    }
+    for (let i = 0; i < options.repeatTimes; i++) {
+      resultChain = [...resultChain, str];
+    }
+    resReturn = resultChain.join(defSeparator);
+  }
+
+  return resReturn;
 }
 
 module.exports = {
